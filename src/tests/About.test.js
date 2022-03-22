a073a5be8d9d773bb70230bef3a1906af97a6cf6
Bryan Fernandes <bryan.efernandes@gmail.com>
2022-03-22 17:27:00 -0300

    Requisito 2

--- src/tests/About.test.js
+++ src/tests/About.test.js
@@ -1,1 +1,25 @@
-test('', () => {});
+import React from 'react';
+import { render, screen } from '@testing-library/react';
+import { About } from '../components';
+
+describe(
+  'Testa se a página contém as informações sobre a Pokédex.', () => {
+    test('Página contém um heading "h2" com o texto: "About Pokédex".', () => {
+      render(<About />);
+
+      const heading = screen.getByRole('heading', { name: /About Pokédex/i, level: 2 });
+      expect(heading).toBeInTheDocument();
+    });
+
+    test('Página contém a imagem de uma Pokédex.', () => {
+      render(<About />);
+      /* Referência: https://dev.to/raphaelchaula/a-simple-image-test-in-react-3p6f */
+
+      const url = 'https://cdn2.bulbagarden.net/upload/thumb/8/86/Gen_I_Pok%C3%A9dex.png/800px-Gen_I_Pok%C3%A9dex.png';
+
+      const img = screen.getByRole('img');
+      expect(img).toHaveAttribute('src', url);
+      expect(img).toHaveAttribute('alt', 'Pokédex');
+    });
+  },
+);
