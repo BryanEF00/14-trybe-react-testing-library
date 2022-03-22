e95b6a0cc73c1296a5f4bfb13c4cdf577695ff25
Bryan Fernandes <bryan.efernandes@gmail.com>
2022-03-22 17:43:30 -0300

    Requisito 3

--- src/tests/FavoritePokemons.test.js
+++ src/tests/FavoritePokemons.test.js
@@ -1,1 +1,14 @@
-test('', () => {});
+import React from 'react';
+import { render, screen } from '@testing-library/react';
+import { FavoritePokemons } from '../components';
+
+describe(
+  'Testa se é exibido na tela a mensagem corretamente.', () => {
+    test('Se a pessoa não tiver pokémons favoritos.', () => {
+      render(<FavoritePokemons />);
+
+      const heading = screen.getByText(/No favorite pokemon found/i);
+      expect(heading).toBeInTheDocument();
+    });
+  },
+);
